2d875849e793f04e3f0023efeb23f1d1cd20a401
DESKTOP-VR8NC81\Nikolina <nikolina.stjepanovic@student.ferit.hr>
2022-02-15 06:22:49 +0100

    ...

--- script/script.js
+++ script/script.js
@@ -16,11 +16,11 @@ $(function() {
     var selectedClass = "";
     $(".category").click(function(){ 
     selectedClass = $(this).attr("data-rel"); 
- $("#portfolio").fadeTo(100, 0.1);
-    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-setTimeout(function() {
-  $("."+selectedClass).fadeIn().addClass('scale-anm');
-  $("#portfolio").fadeTo(300, 1);
+    $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut();
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn();
+    $("#portfolio").fadeTo(300, 1);
 }, 300); 
     
 });
